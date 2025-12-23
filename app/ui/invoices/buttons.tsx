49577042b8a11c0744629516e46cdf7e409f958b
prashant.sahni <prashant.sahni@codeandtheory.com>
2025-12-23 14:10:04 +0530

    Delete invoice

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -1,5 +1,6 @@
 import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import { deleteInvoice } from '@/app/lib/actions';
 
 export function CreateInvoice() {
   return (
@@ -13,6 +14,8 @@ export function CreateInvoice() {
   );
 }
 
+//--------------------------------------------------------------
+
 export function UpdateInvoice({ id }: { id: string }) {
   return (
     <Link
@@ -24,13 +27,18 @@ export function UpdateInvoice({ id }: { id: string }) {
   );
 }
 
+//--------------------------------------------------------------
+
 export function DeleteInvoice({ id }: { id: string }) {
+  const deleteInvoiceWithId = deleteInvoice.bind(null, id);
+
   return (
-    <>
+    <form action={deleteInvoiceWithId}>
       <button type="submit" className="rounded-md border p-2 hover:bg-gray-100">
         <span className="sr-only">Delete</span>
-        <TrashIcon className="w-5" />
+        <TrashIcon className="w-4" />
       </button>
-    </>
+    </form>
   );
 }
+
