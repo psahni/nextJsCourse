792fb9dad5e2be6be11308b418503aca7feac72a
prashant.sahni <prashant.sahni@codeandtheory.com>
2025-12-24 17:39:12 +0530

    Basic Metadata

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -11,6 +11,12 @@ import { useActionState } from 'react';
 import { authenticate } from '@/app/lib/actions';
 import { useSearchParams } from 'next/navigation';
 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Login',
+};
+
 export default function LoginForm() {
   const searchParams = useSearchParams();
   const callbackUrl = searchParams.get('callbackUrl') || '/dashboard';
